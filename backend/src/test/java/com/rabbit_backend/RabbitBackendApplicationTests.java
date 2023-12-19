5076b03b0b1a97fd201fe067f11581b531e2c1b6
Denis Sakhno <sakh0023@algonquinlive.com>
2023-12-19 17:25:06 -0500

    add spring boot bare bones

---
+++ backend/src/test/java/com/rabbit_backend/RabbitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
