bd4c9c76c31628575abc228f9c09b29361654ddb
Denis Sakhno <sakh0023@algonquinlive.com>
2024-01-05 16:49:04 -0500

    upd user model, add first and last name fields

--- backend/src/main/java/com/project_rabbit/User/User.java
+++ backend/src/main/java/com/project_rabbit/User/User.java
@@ -18,6 +18,12 @@ public class User {
     @GeneratedValue
     private Integer userId;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
     @Column(name = "email")
     private String email;
 
