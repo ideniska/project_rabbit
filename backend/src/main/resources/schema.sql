f967bd19a397dc1931173b9321c08052e94ab57f
Denis Sakhno <sakh0023@algonquinlive.com>
2024-01-05 17:07:57 -0500

    delete schema.sql file

--- backend/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
--- users
-CREATE TABLE IF NOT EXISTS users (
-    userId SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NULL,
-    last_name VARCHAR(100) NULL,
-    email VARCHAR(100) NOT NULL,
-    role VARCHAR(100) NOT NULL,
-    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
