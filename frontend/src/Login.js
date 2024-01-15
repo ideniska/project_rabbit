7a58d660a46a13d14b79493138fb15dcaae495af
smit2144 <156718552+smit2144@users.noreply.github.com>
2024-01-15 16:49:36 -0500

    Create login page

---
+++ frontend/src/Login.js
@@ -0,0 +1,42 @@
+// Login.js
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Login:', { username, password });
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Log in
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
